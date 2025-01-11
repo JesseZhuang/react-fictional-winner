9208d50b6d783df00ed26266f60d0bd253be1b21
Jesse Zhuang <JesseZhuang@users.noreply.github.com>
2025-01-11 00:15:55 +0000

    props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
 import FunctionalComponent from './component/FunctionalComponent';
 import Heading from './component/Heading';
+import Bag from './component/Bag';
+import Apples from './component/Apples';
+import Pears from './component/Pears';
 
 function App() {
   return (
@@ -8,6 +11,18 @@ function App() {
       <FunctionalComponent />
       <Heading firstName="Bob" age="30" />
       <Heading firstName="Any name other than Bob" />
+      <Bag children={<Apples color="yellow" number="1" />}>
+        {/* overwrote children above */}
+        <Pears friend="Peter1" />
+      {/* can use self enclosing or separate enclosing html tag */}
+      </Bag>
+      <Bag children={[<Apples color="yellow" number="2" />, <Pears friend="Peter2" />]}>
+      </Bag>
+      <Bag>
+        {/* Apples and Pears are props of Bag, i.e., props.children */}
+        <Apples color="yellow" number="3" />
+        <Pears friend="Peter3" />
+      </Bag>
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
